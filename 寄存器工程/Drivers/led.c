9aee1349e7b61c767d7c07e64a004d87eef3bb7a
Harvey <2524258665@qq.com>
2023-06-20 15:27:01 +0800

    优化寄存器工程

--- 寄存器工程/Drivers/led.c
+++ 寄存器工程/Drivers/led.c
@@ -7,15 +7,15 @@ void led_init(void)
 {
 	RCC->AHB2ENR&=~(1<<1);      //清除原来的设置
 	RCC->AHB2ENR|=1<<1;         //使能 PORTB 口时钟
-	GPIOE->MODER&=~(0x3<<22);
-	GPIOE->MODER|=0x1<<22;      //初始化PB11为输出模式
-	GPIOE->OTYPER&=~(0x1<<11);  //PB11输出推挽
-	GPIOE->OSPEEDR&=~(0x3<<22);
-	GPIOE->OSPEEDR|=0x2<<22;    //PB11高速模式
-	GPIOE->PUPDR&=~(0x3<<22);
-	GPIOE->PUPDR|=0x1<<22;      //PB11上拉
-	GPIOE->ODR&=~(0x1<<11);
-	GPIOE->ODR|=0x1<<11;        //PB11输出1（高电平）
+	GPIOB->MODER&=~(0x3<<22);
+	GPIOB->MODER|=0x1<<22;      //初始化PB11为输出模式
+	GPIOB->OTYPER&=~(0x1<<11);  //PB11输出推挽
+	GPIOB->OSPEEDR&=~(0x3<<22);
+	GPIOB->OSPEEDR|=0x2<<22;    //PB11高速模式
+	GPIOB->PUPDR&=~(0x3<<22);
+	GPIOB->PUPDR|=0x1<<22;      //PB11上拉
+	GPIOB->ODR&=~(0x1<<11);
+	GPIOB->ODR|=0x1<<11;        //PB11输出1（高电平）
 }
 
 
