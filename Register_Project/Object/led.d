ef1441d34a1278a171ef993a0e1f93436598ad4d
Harvey <2524258665@qq.com>
2023-08-07 14:49:40 +0800

    更新寄存器工程
    Signed-off-by: Harvey <2524258665@qq.com>

--- Register_Project/Object/led.d
+++ Register_Project/Object/led.d
@@ -1,11 +1,9 @@
 ../object/led.o: ..\Drivers\led.c ..\Drivers\led.h \
   D:\Keil_v5\Keil\STM32G4xx_DFP\1.5.0\Drivers\CMSIS\Device\ST\STM32G4xx\Include\stm32g431xx.h \
-  D:\Keil_v5\ARM\CMSIS\Include\core_cm4.h \
-  D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  D:\Keil_v5\ARM\CMSIS\Include\cmsis_version.h \
-  D:\Keil_v5\ARM\CMSIS\Include\cmsis_compiler.h \
-  D:\Keil_v5\ARM\CMSIS\Include\cmsis_armclang.h \
+  ..\Include\core_cm4.h D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\Include\cmsis_version.h ..\Include\cmsis_compiler.h \
+  ..\Include\cmsis_armclang.h \
   D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
   D:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
-  D:\Keil_v5\ARM\CMSIS\Include\mpu_armv7.h \
+  ..\Include\mpu_armv7.h \
   D:\Keil_v5\Keil\STM32G4xx_DFP\1.5.0\Drivers\CMSIS\Device\ST\STM32G4xx\Include\system_stm32g4xx.h
